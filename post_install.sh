837b2ba8bee5594a94fbffca623137b17f60c74c
rjgould <robert@gould.me.uk>
2023-05-04 01:58:42 +0100

    Revert "Change post_install.sh"
    This reverts commit 0de06f21dbca273897d257de4a5aec08cb0f4897.

--- post_install.sh
+++ post_install.sh
@@ -4,7 +4,10 @@
 mkdir -p /usr/local/etc/unpackerr
 
 # Fetch Unpackerr release
-fetch https://github.com/Unpackerr/unpackerr/releases/download/v0.11.2/unpackerr.amd64.freebsd.gz -o /usr/local/etc/unpackerr/unpackerr.amd64.freebsd.gz
+fetch https://github.com/Unpackerr/unpackerr/releases/download/v0.11.2/unpackerr.amd64.freebsd.gz -o /tmp/unpackerr.amd64.freebsd.gz
 
 # Extract Unpackerr binary
-gzip -d /usr/local/etc/unpackerr/unpackerr.amd64.freebsd.gz
+gzip -d /tmp/unpackerr.amd64.freebsd.gz
+
+# Move Unpackerr binary
+mv /tmp/unpackerr.amd64.freebsd /usr/local/etc/unpackerr/unpackerr
